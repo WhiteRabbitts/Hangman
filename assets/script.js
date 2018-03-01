7f8ea12252f042b9634602431ff2c563203b19d1
Timothy Swengros <timswengros@Timothys-MacBook-Pro.local>
2018-02-28 19:33:12 -0500

    Testing again

--- assets/script.js
+++ assets/script.js
@@ -49,49 +49,49 @@ var buttons = function () {
         letters.appendChild(list);
     }
 };
-
 result = function () {
-    wordHolder = document.getElementById("hold");
-    correct = document.createElement("ul");
+    wordHolder = document.getElementById('hold');
+    correct = document.createElement('ul');
 
     for (var i = 0; i < word.length; i++) {
-        correct.setAttribute("id", "my-word");
-        guess = document.createElement("li");
-        guess.setAttribute("class", "guess");
+        correct.setAttribute('id', 'my-word');
+        guess = document.createElement('li');
+        guess.setAttribute('class', 'guess');
         if (word[i] === "-") {
             guess.innerHTML = "-";
             space = 1;
         } else {
             guess.innerHTML = "_";
         }
-
+        result();
         guesses.push(guess);
         wordHolder.appendChild(correct);
         correct.appendChild(guess);
     }
 };
 
+
 comments = function () {
     showLives.innerHTML = "You have " + lives + " lives";
     if (lives < 1) {
-      showLives.innerHTML = "Game Over.  See ya real soon!";
+        showLives.innerHTML = "Game Over.  See ya real soon!";
     }
     for (var i = 0; i < guesses.length; i++) {
-      if (counter + space === guesses.length) {
-        showLives.innerHTML = "Good golly miss Molly, you won!";
-      }
+        if (counter + space === guesses.length) {
+            showLives.innerHTML = "Good golly miss Molly, you won!";
+        }
     }
-  }
-
-
-  console.log("javascript");
+};
 
 
 
-    buttons();
-    comments();
+console.log("javascript");
 
 
+
+buttons();
+comments();
+console.log("Javascript");
 
 
 
@@ -113,9 +113,11 @@ var words = [
     "toy-story-mania",
     "peter-pans-flight"
 ];
+
 var word = words[Math.floor(Math.random() * words.length)];
 var answerArray = [];
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
 var remainingLetters = word.length;
+console.log("Javascript");
